e316799ce21d6eff7b6e1abe6cbdb6664245c334
Andrew Jones <Andrew@AHJones-iMac.local>
2017-11-26 20:05:33 -0700

    twitter set

---
+++ keys.js
@@ -0,0 +1,12 @@
+// ```JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: 'cZojSZiRB1ftkWqpfnDfGkrYT',
+    consumer_secret: 'gMsVcPDVUMPjjEU8ho76uQPyjU9GM7kscEKC9s8BKVmOOyW2mT',
+    access_token_key: '934970390115377152-zInEBjUxN1puJbULwss1Re2plMMgsVO',
+    access_token_secret: 'aodWd2Guwzqclr5I5sCgqLJCc9Ytj8mLnWz4nQlEiuQ5H',
+}
+
+module.exports = twitterKeys;
+// ```
